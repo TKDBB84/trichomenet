83664c983feeebc242a44a1b2fb326d1e51bc2da
jeffrey <jeffrey@jeffrey-work-pc>
2012-08-30 10:07:14 -0400

    Login

---
+++ chkUser.php
@@ -0,0 +1,23 @@
+<?php
+if(!isset($_SESSION)) session_start();
+include_once 'connection.php';
+include_once 'Bcrypt.php';
+if(isset($_POST['email']) && isset($_POST['pass'])){
+   $email = $_POST['email'];
+   $supplied_pass = $_POST['pass'];
+   $bcrypt = new Bcrypt(15);
+   $stmt_get_hash = $pdo_dbh->prepare("SELECT `user_id`,`password` FROM `users` WHERE `email` = :email LIMIT 1;");
+   $stmt_get_hash->bindValue(':email',$email,PDO::PARAM_STR);
+   $stmt_get_hash->execute();
+   $row = $stmt_get_hash->fetch(PDO::FETCH_ASSOC);
+   $hash = $row['password'];
+   $user_id = $row['user_id'];
+   $stmt_get_hash->closeCursor();
+   if($bcrypt->verify($supplied_pass, $hash)){
+      $_SESSION['user_id'] = $user_id;
+      header('Location: ./addLeafs.php');
+   }else{
+      $_SESSION['error_text'] = "Invalid Username & Password<br/>";
+      header('Location: ./index.php');
+   }
+ } ?>
--- index.php
+++ index.php
@@ -1,11 +1,20 @@
 <?php
 if(!isset($_SESSION)) session_start();
+if(isset($_SESSION['error_text'])){
+    echo '<span style="color:red;">',$_SESSION['error_text'],'</span>';
+    unset($_SESSION['error_text']);
+}
 ?>
 
+
 Please Login: <br/>
-Username: <input type="text" name="username"/>
+<form method="post" action="chkUser.php">
+Username: <input type="text" name="email"/>
 <br/>
-Password: <input type="password" name="password"/>
+Password: <input type="password" name="pass"/>
+<br/>
+<button type="submit">submit</button>
+</form>
 <br/>
 <br/>
 Or: <a href="./register.php">Register A New User</a>
